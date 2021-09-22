4f2c0a497c38bfdeb1a0859879cf43a63e6815c0
Katie T <68283157+KATIETOLER@users.noreply.github.com>
2021-09-21 20:43:35 -0600

    Add car model

---
+++ object-examples.js
@@ -0,0 +1,16 @@
+var dandelion = {
+
+name: "Dandelion the Great and Horrible",
+age: 3,
+favoriteToys: ["popcorn", "you tried heart", "Mr. Rabbit"],
+dogType: "Jack Russell Terrier Mix",
+
+}
+
+var car = {
+
+carType: "Smart Car?",
+carSize: "Extra small! Very tiny car!",
+wheelNums: 4,
+
+}
